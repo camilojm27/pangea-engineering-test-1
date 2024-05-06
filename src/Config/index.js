273174574d69ce7a740ebe509f27e558673ca01e
Camilo Mezú Mina <camilo272001@gmail.com>
2024-05-06 17:43:13 -0500

    Small improvements on creators detailsPage

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,6 +1,6 @@
 export const ConfigValue = {
   PORT: process.env.PORT || 3000,
-  PUBLIC_REST_API_ENDPOINT: process.env.REACT_APP_BASE_URL,
+  PUBLIC_REST_API_ENDPOINT: "https://blitz-backend-nine.vercel.app/api/",
   AUTH_TOKEN_KEY: "blitz-auth-token",
   AUTH_USER_INFO: "blitz-auth-user",
 };
--- src/Pages/Dashboard/Creators/creatorDetailsPage.js
+++ src/Pages/Dashboard/Creators/creatorDetailsPage.js
@@ -27,11 +27,16 @@ import {
   Pie,
   Cell,
 } from "recharts";
+import { Avatar, Card, CardContent, CardHeader} from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 import blitzLogo from "../../../Components/globalAssets/platty.png";
 import routes from "../../../Config/routes";
 import profilePhoto from "../../../Components/globalAssets/ppfLogo.png"; // Placeholder for the profile photo
 const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+  "https://cdn.jsdelivr.net/npm/world-atlas@2/land-110m.json";
 const mapCountryToIsoA3 = (country) => {
   const countryMap = {
     USA: "USA",
@@ -99,25 +104,25 @@ const CreatorDetailsPage = () => {
   const promotionData = [
     {
       name: "TikTok Sound",
-      value: (creatorDetails.tiktok_sound || ''),
+      value: (creatorDetails.tiktok_sound),
     },
     {
       name: "TikTok Brand",
-      value: (creatorDetails.tiktok_brand || ''),
+      value: (creatorDetails.tiktok_brand),
     },
     {
       name: "Instagram Sound",
-      value: (creatorDetails.instagram_sound || ''),
+      value: (creatorDetails.instagram_sound),
     },
     {
       name: "Instagram Brand",
-      value: (creatorDetails.instagram_brand || ''),
+      value: (creatorDetails.instagram_brand),
     },
   ];
   
 
   return (
-    <>
+    <div>
       <AppBar position="static" sx={{ backgroundColor: "#000" }}>
         <Toolbar>
           <IconButton
@@ -136,99 +141,110 @@ const CreatorDetailsPage = () => {
           {/* Navigation items here, if any */}
         </Toolbar>
       </AppBar>
-      <Box sx={{ flexGrow: 1, padding: 3, backgroundColor: "#f5f5f5" }}>
-  <Grid container spacing={2} justifyContent="center" alignItems="center">
-    <Grid item xs={12} sm={4}>
-      <Box
-        component="img"
-        src={creatorDetails.pfphref || profilePhoto}
-        alt="Profile"
-        sx={{
-          width: '100%',
-          maxWidth: 120,
-          height: 'auto',
-          borderRadius: "50%",
-        }}
-      />
-    </Grid>
-    <Grid item xs={12} sm={8}>
-      <Typography variant="h4" gutterBottom>
-        @{creatorDetails.creator}
-        </Typography>
-        </Grid>
-  </Grid>
-        <Paper sx={{ padding: 2, margin: "20px 0" }}>
-          <Typography variant="body1">
-            <strong>TikTok Profile:</strong>{" "}
-            <a
-              href={creatorDetails.tiktok_link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View TikTok
-            </a>
-          </Typography>
-          <Typography variant="body1">
-            <strong>Instagram Profile:</strong>{" "}
-            <a
-              href={creatorDetails.instagram_link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View Instagram
-            </a>
-          </Typography>
-          <Typography variant="body1">
-            <strong>YouTube Channel:</strong>{" "}
-            <a
-              href={creatorDetails.youtube_link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View YouTube
-            </a>
-          </Typography>
-          {/* Followers Distribution */}
-          <Grid item xs={12} md={6}>
-          <Typography variant="body1">
-              <strong>Presented By:</strong> {creatorDetails.manager}
-            </Typography>
-            <Typography variant="body1">
-              <strong>TikTok Followers:</strong> {creatorDetails.tiktok}
-            </Typography>
-            <Typography variant="body1">
-              <strong>Instagram Followers:</strong> {creatorDetails.instagram}
-            </Typography>
-            <Typography variant="body1">
-              <strong>YouTube Subscribers:</strong> {creatorDetails.youtube}
-            </Typography>
-            <Typography variant="h6" gutterBottom>
-              Followers Distribution
-            </Typography>
-            <ResponsiveContainer width="100%" height={300}>
-              <PieChart>
-                <Pie
-                  data={followersData}
-                  dataKey="value"
-                  nameKey="name"
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={100}
-                  label
-                >
-                  {followersData.map((entry, index) => (
-                    <Cell
-                      key={`cell-${index}`}
-                      fill={["#0088FE", "#00C49F", "#FFBB28"][index]}
-                    />
-                  ))}
-                </Pie>
-                <Tooltip />
-              </PieChart>
-            </ResponsiveContainer>
-          </Grid>
-        </Paper>
 
+      <section>
+        
+        <Grid container justifyContent="center" spacing={6} style={{
+          backgroundColor: "rgb(243 244 246)",
+          padding: "100px",
+          margin: "auto",
+          borderRadius: "15px",
+          boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+
+        }}>
+          <Grid item xs={12} md={6} lg={4} style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "20px",
+            borderRadius: "15px",
+            border: "1px solid #ccc",
+            boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+            backgroundColor: "rgba(255,255,255,0.3)"
+          }}>
+              <Avatar alt="Profile picture" src={creatorDetails.pfphref || profilePhoto}
+ sx={{ width: 120, height: 120 }}>
+                JD
+              </Avatar>
+              <div>
+                <Typography variant="subtitle2" gutterBottom>
+                  Presented By: {creatorDetails.manager}
+
+                </Typography>
+                <Typography variant="body1">
+                  <strong>TikTok Followers:</strong> {creatorDetails.tiktok}
+                </Typography>
+                <Typography variant="body1">
+                  <strong>Instagram Followers:</strong> {creatorDetails.instagram}
+                </Typography>
+                <Typography variant="body1">
+                  <strong>YouTube Subscribers:</strong> {creatorDetails.youtube}
+                </Typography>
+              </div>
+              <Typography variant="body1" color="textSecondary" sx={{ maxWidth: 600 }}>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolore non 
+               magni consectetur iste suscipit ad aut, sequi saepe ullam adipisci, quo deleniti 
+               amet similique dolorum! Harum ab ducimus fuga.
+              </Typography>
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <Grid container direction="row"  justifyContent="space-evenly" alignItems="center" textAlign="center" style={
+              {
+                marginTop: "20px",             }
+            }>
+              <SocialLink icon={<InstagramIcon />} text="Instagram" link={creatorDetails.instagram_link}/>
+              <SocialLink icon={<TwitterIcon />} text="Twitter" link={creatorDetails.twitter_link} />
+              <SocialLink icon={<YouTubeIcon />} text="YouTube" link={creatorDetails.youtube_link} />
+              <SocialLink icon={<YouTubeIcon />} text="TikTok" link={creatorDetails.tiktok_link} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </section>
+      <section style={{
+        padding: "30px",
+      }}>
+        <Grid container spacing={8} justifyContent="center">
+          {[
+            { title: 'Instagram Followers', id: 'Instagram' },
+            { title: 'Twitter Followers', id: 'Twitter' },
+            { title: 'YouTube Subscribers', id: 'YouTube' },
+            { title: 'TikTok Followers', id: 'TikTok' }
+          ].map((item) => (
+            <Grid item xs={12} sm={6} md={4} key={item.id}>
+              <Card elevation={3} style={{
+                borderRadius: "25px"
+              }}>
+                <CardHeader
+                  title={<span style={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#333'}}>{item.title}</span>}
+                />
+                <CardContent>
+                  <ResponsiveContainer width="100%" height={200}>
+                    <PieChart>
+                      <Pie
+                        data={followersData}
+                        dataKey="value"
+                        nameKey="name"
+                        cx="50%"
+                        cy="50%"
+                        outerRadius={100}
+                        label
+                      >
+                        {followersData.map((entry, index) => (
+                          <Cell
+                            key={`cell-${index}`}
+                            fill={["#0088FE", "#00C49F", "#FFBB28"][index % 3]} // Use modulo to repeat colors
+                          />
+                        ))}
+                      </Pie>
+                      <Tooltip />
+                    </PieChart>
+                  </ResponsiveContainer>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
         {/* Promotion Rates */}
         <Paper sx={{ padding: 2, margin: "20px 0" }}>
           <Typography variant="h6" gutterBottom>
@@ -252,44 +268,72 @@ const CreatorDetailsPage = () => {
               </ListItem>
             ))}
           </List>
+          
+        </Paper>
+        
+      </section>
+
+      
+      {/* Additional Details */}
+      <Paper sx={{ padding: 2, margin: "20px 0" }}>
+        <Typography variant="body1">
+          <strong>Geolocation & Ethnicity:</strong>{" "}
+          {creatorDetails.geolocation_gender_ethnicity}
+        </Typography>
+        <Typography variant="body1">
+          <strong>Content Style:</strong> {creatorDetails.notes_content_style}
+        </Typography>
+        <ComposableMap>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const isHighlighted = highlightedCountries.includes(geo.properties.ISO_A3);
+                return isHighlighted ? (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="#FF5533"
+                  />
+                ) : null; // Don't render unhighlighted geographies to clean up the map
+              }).length > 0 ? geographies.map(geo => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#DDD"
+                />
+              )) : <Typography sx={{ textAlign: 'center' }}>Creator can't be mapped.</Typography>
+            }
+          </Geographies>
+         </ComposableMap>
         </Paper>
 
-        {/* Additional Details */}
-        <Paper sx={{ padding: 2, margin: "20px 0" }}>
-          <Typography variant="body1">
-            <strong>Geolocation & Ethnicity:</strong>{" "}
-            {creatorDetails.geolocation_gender_ethnicity}
-          </Typography>
-          <Typography variant="body1">
-            <strong>Content Style:</strong> {creatorDetails.notes_content_style}
-          </Typography>
-          <ComposableMap>
-  <Geographies geography={geoUrl}>
-    {({ geographies }) =>
-      geographies.map((geo) => {
-        const isHighlighted = highlightedCountries.includes(geo.properties.ISO_A3);
-        return isHighlighted ? (
-          <Geography
-            key={geo.rsmKey}
-            geography={geo}
-            fill="#FF5533"
-          />
-        ) : null; // Don't render unhighlighted geographies to clean up the map
-      }).length > 0 ? geographies.map(geo => (
-        <Geography
-          key={geo.rsmKey}
-          geography={geo}
-          fill="#DDD"
-        />
-      )) : <Typography sx={{ textAlign: 'center' }}>Creator can't be mapped.</Typography>
-    }
-  </Geographies>
-</ComposableMap>
-
-        </Paper>
-      </Box>
-    </>
+    </div>
   );
 };
+
+function SocialLink({ icon, text, link }) {
+  return (
+    <>
+      {link && (
+          <Typography  style={{
+              fontSize: "40px",
+          }}>
+            {icon}
+            <a href={link} rel="noopener noreferrer" target="_blank" 
+            style={{
+              fontSize: "30px",
+              color: "gray",
+              hover: "black",
+
+          }}
+            >
+              {text}
+            </a>
+          </Typography>
+      )}
+    </>
+  );
+}
+
 
 export default CreatorDetailsPage;
